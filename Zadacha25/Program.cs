bc29e17794540c06b12dc3e828aec26bbf2e32d2
Marat Yun <tigerplyus@mail.ru>
2022-11-08 19:25:23 +0300

    Решение задачи 25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+int Exponentiation(int A, int B){
+  int result = 1;
+  for(int i=1; i <= B; i++){
+    result = result * A;
+  }
+    return result;
+}
+  Console.Write("Введите число A: ");
+  int numA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int numB = Convert.ToInt32(Console.ReadLine());   
+  Console.WriteLine($"Ответ: {Exponentiation(numA, numB)}");
